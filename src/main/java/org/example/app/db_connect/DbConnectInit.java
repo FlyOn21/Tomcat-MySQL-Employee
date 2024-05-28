1ee6d6d10c40c877d5529c81b0d9d013dc3afebc
Zhoholiev Pavlo <zhogolevpv@gmail.com>
2024-05-28 23:02:37 +0300

    fix db connect setting upload
    28.05.2024 23:02 Zhoholiev Pavlo

--- src/main/java/org/example/app/db_connect/DbConnectInit.java
+++ src/main/java/org/example/app/db_connect/DbConnectInit.java
@@ -15,58 +15,52 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class DbConnectInit implements IConnection {
-    private final Dotenv dotenv;
+//    private final Dotenv dotenv;
     private SessionFactory sessionFactory;
     private final String dbUrl;
     private boolean isConnected;
     @Getter
     private final List<String> connectErrors;
+    private Properties props = new Properties();
     private static final Logger DB_CONNECT_LOGGER =
             LogManager.getLogger(DbConnectInit.class);
 
     public DbConnectInit() {
-        try {
-            this.dotenv = Dotenv.configure().directory(getEnvPath()).load();
-        } catch (Exception e) {
-            DB_CONNECT_LOGGER.error("Failed to load .env file", e);
-            throw new RuntimeException("Failed to load environment settings", e);
-        }
-
-        this.dbUrl = String.format("%s%s:%s/%s", this.dotenv.get("HIBERNATE_JDBC_URL_PREFIX"), this.dotenv.get("MYSQL_HOST"), this.dotenv.get("MYSQL_HOST_PORT"), this.dotenv.get("MYSQL_DATABASE"));
+        getProps();
+        this.dbUrl = String.format("%s%s:%s/%s", props.getProperty("HIBERNATE_JDBC_URL_PREFIX"), props.getProperty("MYSQL_HOST"), props.getProperty("MYSQL_HOST_PORT"), props.getProperty("MYSQL_DATABASE"));
         this.isConnected = false;
         this.connectErrors = new ArrayList<>();
         getSessionFactory();
     }
-    private String getEnvPath() {
-        String catalinaDir = System.getProperty("user.dir");
-        File workDir = new File(catalinaDir).getParentFile();
-        String envFileName = ".env";
-        String projectSpecificPath = "src/main/resources"; // Customize this path as needed
-        File envFile = new File(workDir, projectSpecificPath + File.separator + envFileName);
-        if (!envFile.exists()) {
-            DB_CONNECT_LOGGER.error("Environment file not found at {}", envFile.getAbsolutePath());
-            throw new IllegalStateException("Environment file not found at " + envFile.getAbsolutePath());
+
+    private void getProps() {
+        try {
+            props.load(DbConnectInit.class.getResourceAsStream("/db/db.properties"));
+        } catch (IOException e) {
+            DB_CONNECT_LOGGER.error("Failed to load db.properties file", e);
+            throw new RuntimeException("Failed to load db.properties file", e);
         }
-        return envFile.getParent();  // Return the directory containing the .env file
     }
 
     public Configuration initEnvironment() {
         Configuration configuration = new Configuration();
-        configuration.setProperty("hibernate.connection.driver_class", this.dotenv.get("HIBERNATE_JDBC_DRIVER"));
+        configuration.setProperty("hibernate.connection.driver_class", props.getProperty("HIBERNATE_JDBC_DRIVER"));
         configuration.setProperty("hibernate.connection.url", this.dbUrl);
-        configuration.setProperty("hibernate.connection.username", this.dotenv.get("MYSQL_USER"));
-        configuration.setProperty("hibernate.connection.password", this.dotenv.get("MYSQL_PASSWORD"));
-        configuration.setProperty("hibernate.dialect", this.dotenv.get("HIBERNATE_DIALECT"));
-        configuration.setProperty("hibernate.show_sql", this.dotenv.get("HIBERNATE_SHOW_SQL"));
-        configuration.setProperty("hibernate.format_sql", this.dotenv.get("HIBERNATE_FORMAT_SQL"));
-        configuration.setProperty("hibernate.hbm2ddl.auto", this.dotenv.get("HIBERNATE_HBM2DDL_AUTO"));
-        configuration.setProperty("hibernate.current_session_context_class", this.dotenv.get("HIBERNATE_CURRENT_SESSION_CONTEXT_CLASS"));
-        configuration.setProperty("hibernate.connection.pool_size", this.dotenv.get("HIBERNATE_CONNECTION_POOL_SIZE"));
-        configuration.setProperty("hibernate.generate_statistics", this.dotenv.get("HIBERNATE_GENERATE_STATISTICS"));
+        configuration.setProperty("hibernate.connection.username", props.getProperty("MYSQL_USER"));
+        configuration.setProperty("hibernate.connection.password", props.getProperty("MYSQL_PASSWORD"));
+        configuration.setProperty("hibernate.dialect", props.getProperty("HIBERNATE_DIALECT"));
+        configuration.setProperty("hibernate.show_sql", props.getProperty("HIBERNATE_SHOW_SQL"));
+        configuration.setProperty("hibernate.format_sql", props.getProperty("HIBERNATE_FORMAT_SQL"));
+        configuration.setProperty("hibernate.hbm2ddl.auto", props.getProperty("HIBERNATE_HBM2DDL_AUTO"));
+        configuration.setProperty("hibernate.current_session_context_class", props.getProperty("HIBERNATE_CURRENT_SESSION_CONTEXT_CLASS"));
+        configuration.setProperty("hibernate.connection.pool_size", props.getProperty("HIBERNATE_CONNECTION_POOL_SIZE"));
+        configuration.setProperty("hibernate.generate_statistics", props.getProperty("HIBERNATE_GENERATE_STATISTICS"));
         configuration.addAnnotatedClass(Employee.class);
         return configuration;
     }
